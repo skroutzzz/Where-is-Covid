92ed59898d0720dbd3792ca781c9485241ae98a9
Ioannis-stavrou <stavrougiannis10@gmail.com>
2022-07-13 01:51:30 +0300

    First Completed Site

---
+++ WIC_DB.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE IF NOT EXISTS WIC_DB;
+
+CREATE TABLE IF NOT EXISTS Users (
+
+    userId int(3) PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    userUsername varchar(128) NOT NULL,
+    userPassword varchar(128) NOT NULL,
+    userMail varchar(128) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Admins (
+
+    adminId int(3) PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    adminUsername varchar(128) NOT NULL,
+    adminPasswword varchar(128) NOT NULL,
+    adminMail varchar(128) NOT NULL
+
+)
