2c0ae9b426d1c5f16edb0f9d6b74e9c097f41073
Ioannis-stavrou <stavrougiannis10@gmail.com>
2022-07-13 04:42:46 +0300

    WIC-1 Adding Admins in Users Table

--- WIC_DB.sql
+++ WIC_DB.sql
@@ -9,11 +9,8 @@ CREATE TABLE IF NOT EXISTS Users (
     
 );
 
-CREATE TABLE IF NOT EXISTS Admins (
 
-    adminId int(3) PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    adminUsername varchar(128) NOT NULL,
-    adminPasswword varchar(128) NOT NULL,
-    adminMail varchar(128) NOT NULL
-
-)
+INSERT INTO  Users
+VALUES ('101','Stavrou Ioannis','K!mi123','stavroujohn@gmail.com'),
+        ('201','Konstantinos Tsigkounis','Skr0utzzz%','kontsig@gmail.com'),
+        ('301','Nanos Giorgod','Dvergard12#','nanosgio@gmail.com');
