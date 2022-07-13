742b0c99e221ee489d34cf0bec822d4293c7689d
skroutzzz <konstantinos.tsigk99@gmail.com>
2022-07-13 23:48:51 +0300

    WIC-1 DB template

--- WIC_DB.sql
+++ WIC_DB.sql
@@ -9,8 +9,23 @@ CREATE TABLE IF NOT EXISTS Users (
     
 );
 
+CREATE TABLE IF NOT EXISTS MyPOIS(
+
+);
+
+CREATE TABLE IF NOT EXISTS MyMAP(
+
+);
+
+CREATE TABLE IF NOT EXISTS ICOVID(
+
+);
+
+CREATE TABLE IF NOT EXISTS IVISIT(
+
+);
 
 INSERT INTO  Users
 VALUES ('1','StavrouI','$2y$10$MzwhZvaoupJClUD0VNJzmubGmjBb90K6REAIC.eXEMxvDm0nOXkpm','stavroujohn@gmail.com'),
-        ('2','KonstantinosT','$2y$10$uzReQ0lM52F53emq8IPS.e5BOB0GWvgGli2KxwI/InD73x0R2ZS/u','kontsig@gmail.com'),
-        ('3','NanosG','$2y$10$xGuI4VhsBur5nmpJO4VNGOzfpIMx2Gnv3xE.CKe9heNlzUHzKHEhu','nanosgio@gmail.com');
+       ('2','KonstantinosT','$2y$10$uzReQ0lM52F53emq8IPS.e5BOB0GWvgGli2KxwI/InD73x0R2ZS/u','kontsig@gmail.com'),
+       ('3','NanosG','$2y$10$xGuI4VhsBur5nmpJO4VNGOzfpIMx2Gnv3xE.CKe9heNlzUHzKHEhu','nanosgio@gmail.com');
