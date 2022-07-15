e851d4745a5ee560c5f813e49d961728b79c1928
skroutzzz <konstantinos.tsigk99@gmail.com>
2022-07-15 21:17:58 +0300

    Add Admin Interface

--- admin/includes/navbar.php
+++ admin/includes/navbar.php
@@ -1,3 +1,6 @@
+
+
+
 <!-- Sidebar -->
 <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
@@ -14,7 +17,7 @@
 
 <!-- Nav Item - Dashboard -->
 <li class="nav-item active">
-    <a class="nav-link" href="index.html">
+    <a class="nav-link" href="index.php">
         <i class="fas fa-fw fa-tachometer-alt"></i>
         <span>Dashboard</span></a>
 </li>
@@ -32,16 +35,17 @@
     <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
         aria-expanded="true" aria-controls="collapseTwo">
         <i class="fas fa-fw fa-cog"></i>
-        <span>Components</span>
+        <span>Admin</span>
     </a>
     <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
         <div class="bg-white py-2 collapse-inner rounded">
-            <h6 class="collapse-header">Custom Components:</h6>
-            <a class="collapse-item" href="buttons.html">Buttons</a>
-            <a class="collapse-item" href="cards.html">Cards</a>
+            <h6 class="collapse-header">Admin Options:</h6>
+            <a class="collapse-item" href="./register.php">Add Admin</a>
+            <a class="collapse-item" href="cards.html">Admin Profile</a>
         </div>
     </div>
 </li>
+
 
 <!-- Nav Item - Utilities Collapse Menu -->
 <li class="nav-item">
--- admin/register.php
+++ admin/register.php
@@ -80,6 +80,7 @@ include('includes/navbar.php');
           </div>
         </li>
 
+
         <!-- Nav Item - Alerts -->
         <li class="nav-item dropdown no-arrow mx-1">
           <a
@@ -292,135 +293,95 @@ include('includes/navbar.php');
     </nav>
     <!-- End of Topbar -->
 
-    <!-- Begin Page Content -->
-    <div class="container-fluid">
-      <!-- Page Heading -->
-      <div class="d-sm-flex align-items-center justify-content-between mb-4">
-        <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
-        <a
-          href="#"
-          class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"
-          ><i class="fas fa-download fa-sm text-white-50"></i> Generate
-          Report</a
-        >
+
+<div class="modal fade" id="addadminprofile" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Add Admin Data</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
       </div>
+      <form action="code.php" method="POST">
 
-      <!-- Content Row -->
-      <div class="row">
-        <!-- Earnings (Monthly) Card Example -->
-        <div class="col-xl-3 col-md-6 mb-4">
-          <div class="card border-left-primary shadow h-100 py-2">
-            <div class="card-body">
-              <div class="row no-gutters align-items-center">
-                <div class="col mr-2">
-                  <div
-                    class="text-xs font-weight-bold text-primary text-uppercase mb-1"
-                  >
-                    Earnings (Monthly)
-                  </div>
-                  <div class="h5 mb-0 font-weight-bold text-gray-800">
-                    $40,000
-                  </div>
-                </div>
-                <div class="col-auto">
-                  <i class="fas fa-calendar fa-2x text-gray-300"></i>
-                </div>
-              </div>
+        <div class="modal-body">
+
+            <div class="form-group">
+                <label> Username </label>
+                <input type="text" name="username" class="form-control" placeholder="Enter Username">
             </div>
-          </div>
-        </div>
-
-        <!-- Earnings (Monthly) Card Example -->
-        <div class="col-xl-3 col-md-6 mb-4">
-          <div class="card border-left-success shadow h-100 py-2">
-            <div class="card-body">
-              <div class="row no-gutters align-items-center">
-                <div class="col mr-2">
-                  <div
-                    class="text-xs font-weight-bold text-success text-uppercase mb-1"
-                  >
-                    Earnings (Annual)
-                  </div>
-                  <div class="h5 mb-0 font-weight-bold text-gray-800">
-                    $215,000
-                  </div>
-                </div>
-                <div class="col-auto">
-                  <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
-                </div>
-              </div>
+            <div class="form-group">
+                <label>Email</label>
+                <input type="email" name="email" class="form-control checking_email" placeholder="Enter Email">
+                <small class="error_email" style="color: red;"></small>
             </div>
-          </div>
-        </div>
-
-        <!-- Earnings (Monthly) Card Example -->
-        <div class="col-xl-3 col-md-6 mb-4">
-          <div class="card border-left-info shadow h-100 py-2">
-            <div class="card-body">
-              <div class="row no-gutters align-items-center">
-                <div class="col mr-2">
-                  <div
-                    class="text-xs font-weight-bold text-info text-uppercase mb-1"
-                  >
-                    Tasks
-                  </div>
-                  <div class="row no-gutters align-items-center">
-                    <div class="col-auto">
-                      <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">
-                        50%
-                      </div>
-                    </div>
-                    <div class="col">
-                      <div class="progress progress-sm mr-2">
-                        <div
-                          class="progress-bar bg-info"
-                          role="progressbar"
-                          style="width: 50%"
-                          aria-valuenow="50"
-                          aria-valuemin="0"
-                          aria-valuemax="100"
-                        ></div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div class="col-auto">
-                  <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
-                </div>
-              </div>
+            <div class="form-group">
+                <label>Password</label>
+                <input type="password" name="password" class="form-control" placeholder="Enter Password">
             </div>
-          </div>
-        </div>
-
-        <!-- Pending Requests Card Example -->
-        <div class="col-xl-3 col-md-6 mb-4">
-          <div class="card border-left-warning shadow h-100 py-2">
-            <div class="card-body">
-              <div class="row no-gutters align-items-center">
-                <div class="col mr-2">
-                  <div
-                    class="text-xs font-weight-bold text-warning text-uppercase mb-1"
-                  >
-                    Pending Requests
-                  </div>
-                  <div class="h5 mb-0 font-weight-bold text-gray-800">18</div>
-                </div>
-                <div class="col-auto">
-                  <i class="fas fa-comments fa-2x text-gray-300"></i>
-                </div>
-              </div>
+            <div class="form-group">
+                <label>Confirm Password</label>
+                <input type="password" name="confirmpassword" class="form-control" placeholder="Confirm Password">
             </div>
-          </div>
-        </div>
-      </div>
 
-      <!-- Content Row -->
+
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            <button type="submit" name="registerbtn" class="btn btn-primary">Save</button>
+        </div>
+      </form>
 
     </div>
-    <!-- /.container-fluid -->
   </div>
-  <!-- End of Main Content -->
+</div>
 
+
+
+<div class="container-fluid">
+
+<div class="card shadow mb-4">
+    <div class="card-header py-3">
+        <h6 class="m-0 font-weight-bold text-primary">Admin Profile
+            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#addadminprofile">
+            Add Admin Profile 
+            </button>
+        </h6>
+    </div>
+
+    <div class="card-body">
+    <div class="table-responsive">
+        <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Position</th>
+                    <th>Office</th>
+                    <th>Age</th>
+                    <th>Start Date</th>
+                    <th>Salary</th>
+                </tr>   
+            </thead>
+            <tbody>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
+</div> <!-- PROSOXI AYTO -->
+
+</div>
+
+
+</div>
 
 <?php 
 
