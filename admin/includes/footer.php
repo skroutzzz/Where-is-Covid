c5710d31e1647fc5c609c51f68ab18e3dd5b3231
skroutzzz <konstantinos.tsigk99@gmail.com>
2022-07-15 19:13:14 +0300

    First new interface commit

---
+++ admin/includes/footer.php
@@ -0,0 +1,16 @@
+<!-- Footer -->
+<footer class="sticky-footer bg-white">
+                <div class="container my-auto">
+                    <div class="copyright text-center my-auto">
+                        <span>Copyright &copy; Your Website 2022</span>
+                    </div>
+                </div>
+            </footer>
+            <!-- End of Footer -->
+
+        </div>
+        <!-- End of Content Wrapper -->
+
+</body>
+
+</html>
