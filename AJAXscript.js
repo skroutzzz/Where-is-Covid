09d591ff2149e13f231a740b750c1865dd38e2d8
Dvergar135 <giorgosnanos98@gmail.com>
2022-07-18 22:52:54 +0300

    WIC-8

---
+++ AJAXscript.js
@@ -0,0 +1,14 @@
+const reqListener = response => {
+    const content = document.getElementById("content");
+    content.innerHTML = response.currentTarget.response;
+  };
+  const loadData = () => {
+    const req = new XMLHttpRequest();
+    req.onload = reqListener;
+    req.open("get", "file.txt", true);
+    req.send();
+  };
+  window.onload = () => {
+    const loadButton = document.getElementById("load");
+    loadButton.onclick = loadData;
+  };
