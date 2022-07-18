e784f02e400377261435feff533fa03e29b08ae8
skroutzzz <konstantinos.tsigk99@gmail.com>
2022-07-19 02:03:23 +0300

    WIC-1 Add Admin

--- admin/register.php
+++ admin/register.php
@@ -1,3 +1,163 @@
+
+
+<?php
+// Include config file
+require_once "config.php";
+ 
+// Define variables and initialize with empty values
+$username = $password = $email =$confirm_password =  $admin_id = "";
+$username_err = $password_err = $email_err = $confirm_password_err = "";
+ 
+// Processing form data when form is submitted
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+    
+    //Validate Email
+
+    if(empty(trim($_POST["email"]))){
+        $email_err = "Please enter your email.";
+    } elseif(!preg_match('/^[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i', trim($_POST["email"]))){
+        $email_err = "Try another email.";
+    } else{
+        // Prepare a select statement
+        $sql = "SELECT user_id FROM myUSERS WHERE user_email = ?";
+        
+        if($stmt = mysqli_prepare($link, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "s", $param_email);
+            
+            // Set parameters
+            $param_email = trim($_POST["email"]);
+            
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                /* store result */
+                mysqli_stmt_store_result($stmt);
+                
+                if(mysqli_stmt_num_rows($stmt) == 1){
+                    $email_err = "This email is already taken.";
+                } else{
+                    $email = trim($_POST["email"]);
+                }
+            } else{
+                echo "Oops! Something went wrong. Please try again later. 1";
+            }
+
+            // Close statement
+            mysqli_stmt_close($stmt);
+        }
+    }
+
+ 
+    // Validate username
+    if(empty(trim($_POST["username"]))){
+        $username_err = "Please enter a username.";
+    } elseif(!preg_match('/^[a-zA-Z0-9_]+$/', trim($_POST["username"]))){
+        $username_err = "Username can only contain letters, numbers, and underscores.";
+    } else{
+        // Prepare a select statement
+        $sql = "SELECT user_id FROM myUSERS WHERE user_username = ?";
+        
+        if($stmt = mysqli_prepare($link, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "s", $param_username);
+            
+            // Set parameters
+            $param_username = trim($_POST["username"]);
+            
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                /* store result */
+                mysqli_stmt_store_result($stmt);
+                
+                if(mysqli_stmt_num_rows($stmt) == 1){
+                    $username_err = "This username is already taken.";
+                } else{
+                    $username = trim($_POST["username"]);
+                }
+            } else{
+                echo "Oops! Something went wrong. Please try again later. 2";
+            }
+
+            // Close statement
+            mysqli_stmt_close($stmt);
+        }
+    }
+    
+
+
+     // Validate password
+
+
+     if(empty(trim($_POST["password"]))){
+         $password_err = "Please enter a password.";     
+     }  
+        elseif(strlen(trim($_POST["password"])) < 8 ){
+             $password_err = "Password must have at least 8 characters";
+    }
+      elseif(!preg_match("#[a-z]+#",$_POST["password"])){
+            $password_err = "Password should include at least one low letter";
+    } 
+        elseif(!preg_match("#[A-Z]+#",$_POST["password"])){
+            $password_err = "Password should include at least one upper letter";
+    }
+        elseif(!preg_match("#[0-9]+#",$_POST["password"])){
+            $password_err = "Password should include at least one number";
+    }
+        elseif(!preg_match('@[#$*&!]@', $_POST["password"])){
+            $password_err = "Password should include at least one special character";
+    }
+    else{
+         $password = trim($_POST["password"]);
+     }
+    
+    // Validate confirm password
+    if(empty(trim($_POST["confirm_password"]))){
+        $confirm_password_err = "Please confirm password.";     
+    } else{
+        $confirm_password = trim($_POST["confirm_password"]);
+        if(empty($password_err) && ($password != $confirm_password)){
+            $confirm_password_err = "Password did not match.";
+        }
+    }
+    
+    // Check input errors before inserting in database
+    if(empty($username_err) && empty($password_err) && empty($confirm_password_err) && empty($email_err)){
+        
+   
+        
+        // Prepare an insert statement
+        $sql = "INSERT INTO myUSERS (user_username, user_password, user_email, admin_id) VALUES (?, ?, ?, ?)";
+         
+        if($stmt = mysqli_prepare($link, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "sssi", $param_username, $param_password, $param_email, $param_admin_id);
+            
+            // Set parameters
+            $param_email = $email;
+            $param_username = $username;
+            $param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash
+            $param_admin_id = 1;
+            // $param_admin_id = $admin_id;
+            
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                // Redirect to login page
+                header("location: index.php");
+            } else{
+                echo "Oops! Something went wrong. Please try again later. 3";
+            }
+
+            // Close statement
+            mysqli_stmt_close($stmt);
+        }
+    }
+    
+    // Close connection 
+    mysqli_close($link);
+}
+?>
+
 <?php  
 include('includes/header.php');
 include('includes/navbar.php');
@@ -303,33 +463,33 @@ include('includes/navbar.php');
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
-      <form action="code.php" method="POST">
+      <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" name="signupForm" method="POST">
 
         <div class="modal-body">
 
             <div class="form-group">
                 <label> Username </label>
-                <input type="text" name="username" class="form-control" placeholder="Enter Username">
+                <input type="text" name="username" required class="form-control" placeholder="Enter Username">
             </div>
             <div class="form-group">
                 <label>Email</label>
-                <input type="email" name="email" class="form-control checking_email" placeholder="Enter Email">
+                <input type="email" name="email" required class="form-control checking_email" placeholder="Enter Email">
                 <small class="error_email" style="color: red;"></small>
             </div>
             <div class="form-group">
                 <label>Password</label>
-                <input type="password" name="password" class="form-control" placeholder="Enter Password">
+                <input type="password" name="password" required class="form-control" placeholder="Enter Password">
             </div>
             <div class="form-group">
                 <label>Confirm Password</label>
-                <input type="password" name="confirmpassword" class="form-control" placeholder="Confirm Password">
+                <input type="password" name="confirm_password" required class="form-control" placeholder="Confirm Password">
             </div>
 
 
         </div>
         <div class="modal-footer">
             <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-            <button type="submit" name="registerbtn" class="btn btn-primary">Save</button>
+            <button type="submit" name="signup_button" class="btn btn-primary">Save</button>
         </div>
       </form>
 
