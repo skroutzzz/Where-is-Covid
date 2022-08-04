73c92fd78fe7d90a3376785b8ac0261b04e31d4d
skroutzzz <konstantinos.tsigk99@gmail.com>
2022-08-04 13:41:15 +0300

    Json connect database

--- upload.php
+++ upload.php
@@ -2,24 +2,66 @@
 
 require_once "config.php";
 
-$targetPath = "uploads/" . basename($_FILES["inpFile"]["name"]);
-move_uploaded_file($_FILES["inpFile"]["tmp_name"], $targetPath);
+//$targetPath = "uploads/" . basename($_FILES["inpFile"]["name"]);
+//move_uploaded_file($_FILES["inpFile"]["tmp_name"], $targetPath);
+
+$jsondata = file_get_contents('uploads/generic.json');
+$array = json_decode($jsondata, true);
+
+//INSERT INTO MYPOIS
+
+$stmt = $mysqli -> prepare("
+    INSERT INTO mypois(poi_name, poi_address, rating, rating_n, populartimes)
+    VALUES(?,?,?,?,?)
+
+");
+
+$stmt -> bind_param("ssiis", $poi_name, $poi_address, $rating, $rating_n, $populartimes);
+
+foreach($array as $temp){
+    $poi_name = $temp["name"];
+    $poi_address = $temp["address"];
+    $rating = $temp["rating"];
+    $rating_n = $temp["rating_n"];
+    $populartimes = $temp["populartimes"];
+
+    $stmt -> execute();
+    mysqli_stmt_close($stmt);
+}
 
 
-$json = file_get_contents(FormData());
-$data = json_decode($json);
-echo $data[0];
+//INSERT INTO MYCOORD
+
+$stmt = $mysqli -> prepare("
+    INSERT INTO mycoord(latitude, longtitude)
+    VALUES(?,?)
+");
+
+$stmt -> bind_param("dd", $latitude, $longtitude);
+
+foreach($array as $temp){
+    $latitude = $temp["latitude"];
+    $longtitude = $temp["longtitude"];
+
+    $stmt -> execute();
+    mysqli_stmt_close($stmt);
+}
 
 
+//INSERT INTO MYPOISTYPE
 
-//$filename = inpFile.files[0];  //NOT SURE
-//$data = file_get_contents($filename);
+$stmt = $mysqli -> prepare("
+    INSERT INTO mypois_type(poi_type_name)
+    VALUES(?)
+");
 
-//$array = json_decode($data, true);
+$stmt -> bind_param("s", $poi_type_name);
 
-
-
-
+foreach($array as $temp){
+    $poi_type_name = $temp["types"];
+    $stmt -> execute();
+    mysqli_stmt_close($stmt);
+}
 
 
 ?>
