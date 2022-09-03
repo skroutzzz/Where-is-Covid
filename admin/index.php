aea99bf91cec1f11f5a181127ae916f86d3eec08
skroutzzz <konstantinos.tsigk99@gmail.com>
2022-09-03 20:20:46 +0300

    map

--- admin/index.php
+++ admin/index.php
@@ -77,6 +77,8 @@ require_once "config.php";
             $result_count = mysqli_num_rows($query);
 
             if($result_count>0){
+
+
               
               echo 'Your search for <i> &nbsp;' .$display_words.' </i> <hr /><br />';
               echo $result_count;
@@ -93,8 +95,7 @@ require_once "config.php";
             else
               echo 'No results found';
           }
-          else 
-            echo 'Nope';
+        
 
       ?>
 
@@ -138,163 +139,9 @@ require_once "config.php";
         </li>
 
         <!-- Nav Item - Alerts -->
-        <li class="nav-item dropdown no-arrow mx-1">
-          <a
-            class="nav-link dropdown-toggle"
-            href="#"
-            id="alertsDropdown"
-            role="button"
-            data-toggle="dropdown"
-            aria-haspopup="true"
-            aria-expanded="false"
-          >
-            <i class="fas fa-bell fa-fw"></i>
-            <!-- Counter - Alerts -->
-            <span class="badge badge-danger badge-counter">3+</span>
-          </a>
-          <!-- Dropdown - Alerts -->
-          <div
-            class="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in"
-            aria-labelledby="alertsDropdown"
-          >
-            <h6 class="dropdown-header">Alerts Center</h6>
-            <a class="dropdown-item d-flex align-items-center" href="#">
-              <div class="mr-3">
-                <div class="icon-circle bg-primary">
-                  <i class="fas fa-file-alt text-white"></i>
-                </div>
-              </div>
-              <div>
-                <div class="small text-gray-500">December 12, 2019</div>
-                <span class="font-weight-bold"
-                  >A new monthly report is ready to download!</span
-                >
-              </div>
-            </a>
-            <a class="dropdown-item d-flex align-items-center" href="#">
-              <div class="mr-3">
-                <div class="icon-circle bg-success">
-                  <i class="fas fa-donate text-white"></i>
-                </div>
-              </div>
-              <div>
-                <div class="small text-gray-500">December 7, 2019</div>
-                $290.29 has been deposited into your account!
-              </div>
-            </a>
-            <a class="dropdown-item d-flex align-items-center" href="#">
-              <div class="mr-3">
-                <div class="icon-circle bg-warning">
-                  <i class="fas fa-exclamation-triangle text-white"></i>
-                </div>
-              </div>
-              <div>
-                <div class="small text-gray-500">December 2, 2019</div>
-                Spending Alert: We've noticed unusually high spending for your
-                account.
-              </div>
-            </a>
-            <a class="dropdown-item text-center small text-gray-500" href="#"
-              >Show All Alerts</a
-            >
-          </div>
-        </li>
+        
 
-        <!-- Nav Item - Messages -->
-        <li class="nav-item dropdown no-arrow mx-1">
-          <a
-            class="nav-link dropdown-toggle"
-            href="#"
-            id="messagesDropdown"
-            role="button"
-            data-toggle="dropdown"
-            aria-haspopup="true"
-            aria-expanded="false"
-          >
-            <i class="fas fa-envelope fa-fw"></i>
-            <!-- Counter - Messages -->
-            <span class="badge badge-danger badge-counter">7</span>
-          </a>
-          <!-- Dropdown - Messages -->
-          <div
-            class="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in"
-            aria-labelledby="messagesDropdown"
-          >
-            <h6 class="dropdown-header">Message Center</h6>
-            <a class="dropdown-item d-flex align-items-center" href="#">
-              <div class="dropdown-list-image mr-3">
-                <img
-                  class="rounded-circle"
-                  src="img/undraw_profile_1.svg"
-                  alt="..."
-                />
-                <div class="status-indicator bg-success"></div>
-              </div>
-              <div class="font-weight-bold">
-                <div class="text-truncate">
-                  Hi there! I am wondering if you can help me with a problem
-                  I've been having.
-                </div>
-                <div class="small text-gray-500">Emily Fowler · 58m</div>
-              </div>
-            </a>
-            <a class="dropdown-item d-flex align-items-center" href="#">
-              <div class="dropdown-list-image mr-3">
-                <img
-                  class="rounded-circle"
-                  src="img/undraw_profile_2.svg"
-                  alt="..."
-                />
-                <div class="status-indicator"></div>
-              </div>
-              <div>
-                <div class="text-truncate">
-                  I have the photos that you ordered last month, how would you
-                  like them sent to you?
-                </div>
-                <div class="small text-gray-500">Jae Chun · 1d</div>
-              </div>
-            </a>
-            <a class="dropdown-item d-flex align-items-center" href="#">
-              <div class="dropdown-list-image mr-3">
-                <img
-                  class="rounded-circle"
-                  src="img/undraw_profile_3.svg"
-                  alt="..."
-                />
-                <div class="status-indicator bg-warning"></div>
-              </div>
-              <div>
-                <div class="text-truncate">
-                  Last month's report looks great, I am very happy with the
-                  progress so far, keep up the good work!
-                </div>
-                <div class="small text-gray-500">Morgan Alvarez · 2d</div>
-              </div>
-            </a>
-            <a class="dropdown-item d-flex align-items-center" href="#">
-              <div class="dropdown-list-image mr-3">
-                <img
-                  class="rounded-circle"
-                  src="https://source.unsplash.com/Mv9hjnEUHR4/60x60"
-                  alt="..."
-                />
-                <div class="status-indicator bg-success"></div>
-              </div>
-              <div>
-                <div class="text-truncate">
-                  Am I a good boy? The reason I ask is because someone told me
-                  that people say this to all dogs, even if they aren't good...
-                </div>
-                <div class="small text-gray-500">Chicken the Dog · 2w</div>
-              </div>
-            </a>
-            <a class="dropdown-item text-center small text-gray-500" href="#"
-              >Read More Messages</a
-            >
-          </div>
-        </li>
-
+       
         <div class="topbar-divider d-none d-sm-block"></div>
 
         <!-- Nav Item - User Information -->
@@ -352,7 +199,7 @@ require_once "config.php";
     <div class="container-fluid">
       <!-- Page Heading -->
       <div class="d-sm-flex align-items-center justify-content-between mb-4">
-        <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
+      <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
         <a
           href="#"
           class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"
@@ -361,115 +208,55 @@ require_once "config.php";
         >
       </div>
 
-      <!-- Content Row -->
-      <div class="row">
-        <!-- Earnings (Monthly) Card Example -->
-        <div class="col-xl-3 col-md-6 mb-4">
-          <div class="card border-left-primary shadow h-100 py-2">
-            <div class="card-body">
-              <div class="row no-gutters align-items-center">
-                <div class="col mr-2">
-                  <div
-                    class="text-xs font-weight-bold text-primary text-uppercase mb-1"
-                  >
-                    Earnings (Monthly)
-                  </div>
-                  <div class="h5 mb-0 font-weight-bold text-gray-800">
-                    $40,000
-                  </div>
-                </div>
-                <div class="col-auto">
-                  <i class="fas fa-calendar fa-2x text-gray-300"></i>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+      <div id="map">
+      <a
+        href="https://www.maptiler.com"
+        style="position: absolute; left: 10px; bottom: 10px; z-index: 999"
+        ><img
+          src="https://api.maptiler.com/resources/logo.svg"
+          alt="MapTiler logo"
+      /></a>
+    </div>
+    <p>
+      <a href="https://www.maptiler.com/copyright/" target="_blank"
+        >&copy; MapTiler</a
+      >
+      <a href="https://www.openstreetmap.org/copyright" target="_blank"
+        >&copy; OpenStreetMap contributors</a
+      >
+    </p>
+    <script>
+      var map = L.map("map").setView([0, 0], 1);
+      L.tileLayer(
+        "https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=m4UDU1L0NSzWbDH7I0Ir",
+        {
+          tileSize: 512,
+          zoomOffset: -1,
+          minZoom: 15,
+          attribution:
+            '\u003ca href="https://www.maptiler.com/copyright/" target="_blank"\u003e\u0026copy; MapTiler\u003c/a\u003e \u003ca href="https://www.openstreetmap.org/copyright" target="_blank"\u003e\u0026copy; OpenStreetMap contributors\u003c/a\u003e',
+          crossOrigin: true,
+        }
+      ).addTo(map);
 
-        <!-- Earnings (Monthly) Card Example -->
-        <div class="col-xl-3 col-md-6 mb-4">
-          <div class="card border-left-success shadow h-100 py-2">
-            <div class="card-body">
-              <div class="row no-gutters align-items-center">
-                <div class="col mr-2">
-                  <div
-                    class="text-xs font-weight-bold text-success text-uppercase mb-1"
-                  >
-                    Earnings (Annual)
-                  </div>
-                  <div class="h5 mb-0 font-weight-bold text-gray-800">
-                    $215,000
-                  </div>
-                </div>
-                <div class="col-auto">
-                  <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+      map.locate({ setView: true, maxZoom: 16 });
+      function onLocationFound(e) {
+        var radius = e.accuracy;
 
-        <!-- Earnings (Monthly) Card Example -->
-        <div class="col-xl-3 col-md-6 mb-4">
-          <div class="card border-left-info shadow h-100 py-2">
-            <div class="card-body">
-              <div class="row no-gutters align-items-center">
-                <div class="col mr-2">
-                  <div
-                    class="text-xs font-weight-bold text-info text-uppercase mb-1"
-                  >
-                    Tasks
-                  </div>
-                  <div class="row no-gutters align-items-center">
-                    <div class="col-auto">
-                      <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">
-                        50%
-                      </div>
-                    </div>
-                    <div class="col">
-                      <div class="progress progress-sm mr-2">
-                        <div
-                          class="progress-bar bg-info"
-                          role="progressbar"
-                          style="width: 50%"
-                          aria-valuenow="50"
-                          aria-valuemin="0"
-                          aria-valuemax="100"
-                        ></div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div class="col-auto">
-                  <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+        L.marker(e.latlng)
+          .addTo(map)
+          .bindPopup("You are within 20 meters from this point")
+          .openPopup();
 
-        <!-- Pending Requests Card Example -->
-        <div class="col-xl-3 col-md-6 mb-4">
-          <div class="card border-left-warning shadow h-100 py-2">
-            <div class="card-body">
-              <div class="row no-gutters align-items-center">
-                <div class="col mr-2">
-                  <div
-                    class="text-xs font-weight-bold text-warning text-uppercase mb-1"
-                  >
-                    Pending Requests
-                  </div>
-                  <div class="h5 mb-0 font-weight-bold text-gray-800">18</div>
-                </div>
-                <div class="col-auto">
-                  <i class="fas fa-comments fa-2x text-gray-300"></i>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+        L.circle(e.latlng, 10, { color: "red" }).addTo(map);
+      }
 
+      map.on("locationfound", onLocationFound);
+
+      marker.bindPopup("<b> Hey there! </b><br>I am a marker.").openPopup();
+      circle.bindPopup("I am a circle");
+      polygon.bindPopup("I am polygon");
+    </script>
       <!-- Content Row -->
 
     </div>
