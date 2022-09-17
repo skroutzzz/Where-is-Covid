d1b3069fb8cad420df16fb87d2a729aa2385e474
Ioannis-stavrou <stavrougiannis10@gmail.com>
2022-09-18 02:45:34 +0300

    Revert "UploadibgUI"
    This reverts commit 809b9951b34ea886f173a60131cfb3f0f6203290.

--- admin/insertdata.php
+++ admin/insertdata.php
@@ -1,135 +1,227 @@
+<?php
+// Initialize the session
+session_start();
+ 
+?>
+
+<?php  
+include('includes/header.php');
+include('includes/navbar.php');
+
+?>
+
+
+<!-- Content Wrapper -->
+<div id="content-wrapper" class="d-flex flex-column">
+  <!-- Main Content -->
+  <div id="content">
+    <!-- Topbar -->
+    <nav
+      class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow"
+    >
+      <!-- Sidebar Toggle (Topbar) -->
+      <button
+        id="sidebarToggleTop"
+        class="btn btn-link d-md-none rounded-circle mr-3"
+      >
+        <i class="fa fa-bars"></i>
+      </button>
+
+      <!-- Topbar Search -->
+      <form
+        class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search"
+      >
+        <div class="input-group">
+          <input
+            type="text"
+            class="form-control bg-light border-0 small"
+            placeholder="Search for..."
+            aria-label="Search"
+            aria-describedby="basic-addon2"
+          />
+          <div class="input-group-append">
+            <button class="btn btn-primary" type="button">
+              <i class="fas fa-search fa-sm"></i>
+            </button>
+          </div>
+        </div>
+      </form>
+
+      <!-- Topbar Navbar -->
+      <ul class="navbar-nav ml-auto">
+        <!-- Nav Item - Search Dropdown (Visible Only XS) -->
+        <li class="nav-item dropdown no-arrow d-sm-none">
+          <a
+            class="nav-link dropdown-toggle"
+            href="#"
+            id="searchDropdown"
+            role="button"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+          >
+            <i class="fas fa-search fa-fw"></i>
+          </a>
+          <!-- Dropdown - Messages -->
+          <div
+            class="dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in"
+            aria-labelledby="searchDropdown"
+          >
+            <form class="form-inline mr-auto w-100 navbar-search">
+              <div class="input-group">
+                <input
+                  type="text"
+                  class="form-control bg-light border-0 small"
+                  placeholder="Search for..."
+                  aria-label="Search"
+                  aria-describedby="basic-addon2"
+                />
+                <div class="input-group-append">
+                  <button class="btn btn-primary" type="button">
+                    <i class="fas fa-search fa-sm"></i>
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </li>
+
+
+        <div class="topbar-divider d-none d-sm-block"></div>
+
+        <!-- Nav Item - User Information -->
+        <li class="nav-item dropdown no-arrow">
+          <a
+            class="nav-link dropdown-toggle"
+            href="#"
+            id="userDropdown"
+            role="button"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+          >
+            <span class="mr-2 d-none d-lg-inline text-gray-600 small"
+              ><b><?php echo htmlspecialchars($_SESSION["username"]); ?></b></span
+            >
+            <img
+              class="img-profile rounded-circle"
+              src="img/undraw_profile.svg"
+            />
+          </a>
+          <!-- Dropdown - User Information -->
+          <div
+            class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
+            aria-labelledby="userDropdown"
+          >
+            <div class="dropdown-divider"></div>
+            <a
+              class="dropdown-item"
+              href="#"
+              data-toggle="modal"
+              data-target="#logoutModal"
+            >
+              <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+              Logout
+            </a>
+          </div>
+        </li>
+      </ul>
+    </nav>
+    <!-- End of Topbar -->
+
+  <div class="modal fade" id="addadminprofile" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Insert Data</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+         <form class="form" id="myForm">
+        
+          <div class="modal-body">
+
+              <div class="form-group">
+                <input type="file" id="inpFile" /> <br />
+
+                <button type="submit" id="btn">Upload File</button>
+                </div>  
+                <button id="load">Fetch Info</button>
+    <script
+      src="https://code.jquery.com/jquery-3.6.0.min.js"
+      integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
+      crossorigin="anonymous"
+    ></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.bundle.min.js"></script>
+    <script>
+      const myForm = document.getElementById("myForm");
+      const inpFile = document.getElementById("inpFile");
+
+      myForm.addEventListener("submit", (e) => {
+        e.preventDefault();
+
+        console.log(inpFile.files);
+
+        var fileReader = new FileReader();
+        var file;
+        fileReader.readAsText(inpFile.files[0]);
+        fileReader.onload = function () {
+          file = JSON.parse(fileReader.result);
+          console.log(file);
+        };
+
+        fileReader.onloadend = function () {
+          var formData = JSON.stringify(file);
+          $.ajax({
+            url: "upload.php",
+            dataType: "text",
+            type: "POST",
+            data: { formData: formData },
+            success: function (response) {
+              // window.location = "../index.php"
+              console.log(response);
+            },
+            error: function (error) {
+              console.log(error);
+            },
+          });
+        };
+      });
+    </script>
+    <div id="content"></div>
+
+             </div>
+         </form>
+
+      </div>
+    </div>
+  </div>
+
+
+
+
+<div class="container-fluid">
+
+<div class="card shadow mb-4">
+    <div class="card-header py-3">
+            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#addadminprofile">
+            Insert Data
+            </button>
+        </h6>
+    </div>
+    <div class="card-body">
+    <div class="table-responsive">
+
+</div> <!-- PROSOXI AYTO -->
+
+</div>
+
+
+</div>
+
 <?php 
 
-require_once "config.php";
-
-header('Content-Type: text/plain');
-
-if(isset($_POST['formData'])){
-    $received = utf8_encode($_POST['formData']);
-    $pois = json_decode($received);
-   // print_r($pois);
- 
-    
-
-    foreach($pois as $temp){
-        $poi_id = $temp->id;
-        $poi_name = $temp->name;
-        $poi_address = $temp->address;
-        $rating = $temp->rating;
-        $rating_n = $temp->rating_n;
-        $populartimes = json_encode($temp->populartimes);
-        $latitude = $temp->coordinates->lat;
-        $longtitude = $temp->coordinates->lng;
-        //$types = json_decode($temp->types);
-        
-        $poi_types = json_encode($temp->types);
-        $i = 0;
-        foreach($poi_types as $types){
-            $poi_types = $types[$i];
-            $i++;
-        }
-       
-       $insert_pois = "INSERT INTO mypois(poi_id, poi_name, poi_address, rating, rating_n, populartimes, latitude, longtitude)
-        VALUES ('$poi_id', '$poi_name', '$poi_address', '$rating', '$rating_n', '$populartimes', $latitude, $longtitude);";
-
-       $insert_types = "INSERT INTO mypois_type(poi_type_id, poi_type_name)
-       VALUES ('$poi_id','$poi_types');";
-     
-     try {
-        $stmt = mysqli_stmt_init($link);
-        mysqli_stmt_prepare($stmt, $insert_pois);
-        mysqli_stmt_execute($stmt);
-        mysqli_stmt_close($stmt);
-        echo "[SQL Success]";
-        $stmt = mysqli_stmt_init($link);
-        mysqli_stmt_prepare($stmt, $insert_types);
-        mysqli_stmt_execute($stmt);
-        mysqli_stmt_close($stmt);
-        echo "[SQL Success]";
-    }
-    catch (Exception $err) {
-        echo "[SQL Failed]";
-        die;
-    }
-
-    }
-
-
-   
-}
-
-
-
-
-/*
-
-
-$targetPath = "uploads/" . basename($_FILES["inpFile"]["name"]);
-move_uploaded_file($_FILES["inpFile"]["tmp_name"], $targetPath);
-
-$mygetter = $_POST["FormData"];
-
-$jsondata = file_get_contents($mygetter);
-//$jsondata = file_get_contents('uploads/generic.json');
-$array = json_decode($jsondata, true);
-print_r($array);
-
-//INSERT INTO MYPOIS
-
-$stmt = $mysqli -> prepare("
-    INSERT INTO mypois(poi_name, poi_address, rating, rating_n, populartimes)
-    VALUES(?,?,?,?,?)
-
-");
-
-$stmt -> bind_param("ssiis", $poi_name, $poi_address, $rating, $rating_n, $populartimes);
-
-foreach($array as $temp){
-    $poi_name = $temp["name"];
-    $poi_address = $temp["address"];
-    $rating = $temp["rating"];
-    $rating_n = $temp["rating_n"];
-    $populartimes = $temp["populartimes"];
-
-    $stmt -> execute();
-    $mysqli -> close();
-}
-
-*/
-
-/*
-//INSERT INTO MYCOORD
-
-$stmt = $mysqli -> prepare("
-    INSERT INTO mycoord(latitude, longtitude)
-    VALUES(?,?)
-");
-
-$stmt -> bind_param("dd", $latitude, $longtitude);
-
-foreach($array as $temp){
-    $latitude = $temp["latitude"];
-    $longtitude = $temp["longtitude"];
-
-    $stmt -> execute();
-    $mysqli -> close();
-}
-
-
-//INSERT INTO MYPOISTYPE
-
-$stmt = $mysqli -> prepare("
-    INSERT INTO mypois_type(poi_type_name)
-    VALUES(?)
-");
-
-$stmt -> bind_param("s", $poi_type_name);
-
-foreach($array as $temp){
-    $poi_type_name = $temp["types"];
-    $stmt -> execute();
-    $mysqli -> close();
-}
-
-*/
+ include('includes/footer.php');
+ include('includes/scripts.php'); 
 ?>
