f3066b0a4d2350cdb8da30b099feaac4d64a5a09
skroutzzz <konstantinos.tsigk99@gmail.com>
2022-09-18 17:04:25 +0300

    Navbar specials

--- admin/register.php
+++ admin/register.php
@@ -241,18 +241,7 @@ include('includes/navbar.php');
             class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
             aria-labelledby="userDropdown"
           >
-            <a class="dropdown-item" href="#">
-              <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
-              Profile
-            </a>
-            <a class="dropdown-item" href="#">
-              <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
-              Settings
-            </a>
-            <a class="dropdown-item" href="#">
-              <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
-              Activity Log
-            </a>
+         
             <div class="dropdown-divider"></div>
             <a
               class="dropdown-item"
